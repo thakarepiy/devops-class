d0c920833b45bcf8213d424a859c2ad960d8dc4f
thakarepiy <thakarepiyush@gmail.com>
2023-03-16 13:27:29 +0530

    code push

---
+++ src/main/java/com/example/devopsclass/DevopsClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsClassApplication.class, args);
+	}
+
+}
